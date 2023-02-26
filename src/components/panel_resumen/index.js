9dd6e8e19df1dce4da018963cb2d48902cf8cc8c
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-02-26 18:25:41 -0500

    add travel options

--- src/components/panel_resumen/index.js
+++ src/components/panel_resumen/index.js
@@ -20,22 +20,43 @@ export function PanelResumen() {
 				</thead>
 			</table>
 			<table className={styles.resumenTable}>
-				<caption>Información del viaje</caption>
+				<caption>Boletos</caption>
 				<thead>
 					<tr>
-						<th>Número de bus</th>
-						<td>78</td>
+						<th>Vendidos</th>
+						<td>23</td>
 					</tr>
 					<tr>
-						<th>Viaje actual</th>
-						<td>Santo Domingo - Manta</td>
+						<th>anulados</th>
+						<td>2</td>
 					</tr>
 					<tr>
-						<th>Salida</th>
-						<td>10:00</td>
+						<th>Disponibles</th>
+						<td>17</td>
 					</tr>
 				</thead>
 			</table>
+			<table className={styles.resumenTable}>
+				<caption>Ventas</caption>
+				<thead>
+					<tr>
+						<th>Recaudado</th>
+						<td>$20.00</td>
+					</tr>
+					<tr>
+						<th>Ahorrado</th>
+						<td>--</td>
+					</tr>
+					<tr>
+						<th>Total</th>
+						<td>$20.00</td>
+					</tr>
+				</thead>
+			</table>
+			<div className={styles.groupBtn}>
+				<button>Cerrar viaje</button>
+				<small>Cambiar viaje</small>
+			</div>
 		</div>
 	);
 }
