d9b24c3961f52cb130fd8a8b0cb9c1b4e1f85e4c
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-02-26 11:20:23 -0500

    fix bug context

--- src/context/Signin/index.js
+++ src/context/Signin/index.js
@@ -1,13 +1,12 @@
 import { createContext, useState } from "react";
 
 const SigninContext = createContext();
-const initialState = false;
 
 const SigninProvider = ({ children }) => {
-	const [isSignin, setSignin] = useState(initialState);
+	const [isSignin, setSignin] = useState(false);
 	const handleAuth = (e) => setSignin(true);
-
 	const data = { isSignin, handleAuth };
+
 	return <SigninContext.Provider value={data}>{children}</SigninContext.Provider>;
 };
 
