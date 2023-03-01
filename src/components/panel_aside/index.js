c3c30204a272e3f75c8a0d949688429d16befb0b
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-02-28 23:33:35 -0500

    add create travel

--- src/components/panel_aside/index.js
+++ src/components/panel_aside/index.js
@@ -1,6 +1,7 @@
 import styles from "./index.module.css";
 import { PanelSectionContext } from "@/context/panel_section";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { Modal } from "../modal";
 
 export function PanelAside() {
 	const {
@@ -12,28 +13,125 @@ export function PanelAside() {
 		isReportActive,
 	} = useContext(PanelSectionContext);
 
+	const [isModalActive, setModalActive] = useState(false);
+
+	const handleCreateTravel = () => {
+		setModalActive(isModalActive ? false : true);
+	};
+
 	return (
-		<aside className={styles.aside}>
-			<ul>
-				<li
-					onClick={handleTicketActive}
-					className={styles[isTicketActive ? "asideActive" : ""]}
-				>
-					Boleteria
-				</li>
-				<li
-					onClick={handlePackageActive}
-					className={styles[isPackageActive ? "asideActive" : ""]}
-				>
-					Encomiendas
-				</li>
-				<li
-					onClick={handleReportActive}
-					className={styles[isReportActive ? "asideActive" : ""]}
-				>
-					Reportes
-				</li>
-			</ul>
-		</aside>
+		<>
+			<aside className={styles.aside}>
+				<ul>
+					<li className={styles.create}>
+						Crear viaje <span class="material-symbols-outlined">add_circle</span>
+					</li>
+					<li
+						onClick={handleTicketActive}
+						className={styles[isTicketActive ? "asideActive" : ""]}
+					>
+						Boleteria
+					</li>
+					<li
+						onClick={handlePackageActive}
+						className={styles[isPackageActive ? "asideActive" : ""]}
+					>
+						Encomiendas
+					</li>
+					<li
+						onClick={handleReportActive}
+						className={styles[isReportActive ? "asideActive" : ""]}
+					>
+						Reportes
+					</li>
+				</ul>
+			</aside>
+			<Modal>
+				<form className={styles.createForm}>
+					<fieldset>
+						<legend>
+							<h3>Crear viaje</h3>
+							<small>Aquí puedes consultar las frecuencia disponibles</small>
+						</legend>
+						<select>
+							<option>Selecciona una opción</option>
+							<option>Boleteria</option>
+							<option>Encomienda</option>
+						</select>
+						<div className={styles.formDate}>
+							<label>Fecha</label>
+							<input type="date" />
+						</div>
+						<table>
+							<thead>
+								<tr>
+									<th>Bus</th>
+									<th>Ruta</th>
+									<th>hora</th>
+									<th>Crear</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Santo Domingo - El Carmen</td>
+									<td>14:00</td>
+									<td>
+										<span class="material-symbols-outlined">add_circle</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div className={styles.createFormPages}>
+							{/* <span class="material-symbols-outlined">navigate_before</span> */}
+							<small>Anterior</small>
+							<small>1 ... 7</small>
+							<small>Siguiente</small>
+						</div>
+						<button>Cerrar</button>
+					</fieldset>
+				</form>
+			</Modal>
+		</>
 	);
 }
